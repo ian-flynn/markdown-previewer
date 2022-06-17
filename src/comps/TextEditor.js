3ee501cbfb9e58cd41450ef6ac3736a02dae4cf0
ianflynnwork <ianflynnwork@gmail.com>
2022-06-17 19:15:07 -0400

    ,

--- src/comps/TextEditor.js
+++ src/comps/TextEditor.js
@@ -5,7 +5,7 @@ const TextEditor = ({editorText, setEditorText}) => {
         setEditorText(e.target.value);
     }
     return (
-        <div>
+        <div className="editor-box">
             <div>text editor - h1 h2 link inlinecode codeblock listitem blockquote image boldedtext</div>
              <textarea id="editor" onChange={handleChange} placeholder={editorText}/>
         </div>
