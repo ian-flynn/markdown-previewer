3c497ed8f9bd5fc01d0cbedc3ee5624944af031b
ianflynnwork <ianflynnwork@gmail.com>
2022-06-20 17:57:47 -0400

    css stylings

--- src/App.js
+++ src/App.js
@@ -6,47 +6,46 @@ import TextEditor from "./comps/TextEditor";
 function App() {
   const initialInput =`# Welcome to my React Markdown Previewer!
 
-  ## This is a sub-heading...
-  ### And here's some other cool stuff:
+## Here are some mardown examples for you to try...
   
-  Heres some code, \`<div></div>\`, between 2 backticks.
+Heres some code, \`<div></div>\`, between 2 backticks.
   
-  \`\`\`
-  // this is multi-line code:
+\`\`\`
+// this is multi-line code:
   
-  function anotherExample(firstLine, lastLine) {
-    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-      return multiLineCode;
-    }
-  }
-  \`\`\`
+function myFunction(p1, p2) {
+  return p1 * p2;   // The function returns the product of p1 and p2
+}
+\`\`\`
   
-  You can also make text **bold**... whoa!
+  You can make text **bold**...
   Or _italic_.
-  Or... wait for it... **_both!_**
-  And feel free to go crazy ~~crossing stuff out~~.
+  Or **_both!_**
   
-  There's also [links](https://www.freecodecamp.org), and
-  > Block Quotes!
+ You can ~~crossing stuff out~~.
   
-  And if you want to get really crazy, even tables:
+  Here is a [link](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) (to a cheatsheet), and
+  > A Block Quote!
   
-  Wild Header | Crazy Header | Another Header?
-  ------------ | ------------- | -------------
-  Your content can | be here, and it | can be here....
-  And here. | Okay. | I think we get it.
+ You can make a table:
+
+  | Tables        | Are           | Cool  |
+  | ------------- |:-------------:| -----:|
+  | col 3 is      | right-aligned | $1600 |
+  | col 2 is      | centered      |   $12 |
+  | zebra stripes | are neat      |    $1 |
+
+  - You can make a list
+    - Indentation
+       - Changes
+          - Things
   
-  - And of course there are lists.
-    - Some are bulleted.
-       - With different indentation levels.
-          - That look like this.
   
+  1. Make a numbered list
+  1. The numbers don't matter
+  1. Imbed an image!:
   
-  1. And there are numbered lists too.
-  1. Use just 1s if you want!
-  1. And last but not least, let's not forget embedded images:
-  
-  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  ![Photo of a beautiful dog](https://www.k9ofmine.com/wp-content/uploads/2018/07/Most-beautiful-dogs.jpg)
   `;
   const [editorText, setEditorText] = useState(initialInput);
   
--- src/comps/Previewer.js
+++ src/comps/Previewer.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+
 const Previewer = ({ editorText }) => {
 
     return (
         <div className="wrapper-box">
             <div className="box-title">Previewer</div>
             <div id="preview">
-                <ReactMarkdown children={ editorText } remarkPlugins={[ remarkGfm ]} />
+                <ReactMarkdown children={ editorText } remarkPlugins={[remarkGfm]} />
             </div>
 
 
